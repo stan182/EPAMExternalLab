46cc9e8db10bb755af36cdcb92f872c3101b141f
stan182 <stanislav.emshanov@gmail.com>
2017-12-11 00:59:32 +0400

    add homework html advance 2

---
+++ HTML Advance Homework/main.js
@@ -0,0 +1,8 @@
+//var cautionPlayer = document.getElementById("caution-player");
+//cautionPlayer.addEventListener("play", function(){
+//    alert("Ты нажал на Play") 
+//})
+//
+//cautionPlayer.addEventListener("pause", function(){
+//    alert("Ты нажал на Pause") 
+//})
