815f844fb29f6f2325425c1b14f8722f7f9326c7
stan182 <stanislav.emshanov@gmail.com>
2018-02-02 22:05:54 +0400

    add hw js advanced
    5/5

--- JS Advanced/homework.js
+++ JS Advanced/homework.js
@@ -20,6 +20,7 @@ function compare(value1, value2) {
 }
 
 console.log(compare(2, 1));
+console.log(compare(3, 3));
 console.log(compare(1, 5));
 console.log(compare(2));
 console.log(compare(1, '2'));
@@ -36,13 +37,15 @@ function random(sumWith) {
   })
 }
 
-random(10)
+random(3)
   .then(function(x) {
     return random(x);
   })
   .then(function(x) {
     return random(x);
 });
+
+console.log(random(3));
 
 //Для задания 'b':
 function random() {
@@ -86,21 +89,74 @@ function counter () {
 
 var counter = counter(); 
 
-console.log('Счетчик увеличивается до ', counter.next());
-console.log('Счетчик увеличивается до ', counter.next());
-console.log('Счетчик увеличивается до ', counter.next());
-console.log('Счетчик увеличивается до ', counter.next());
-console.log('Счетчик увеличивается до ', counter.next());
-console.log('Счетчик уменьшается до ', counter.prev());
-console.log('Счетчик уменьшается до ', counter.prev());
-console.log('Счетчик уменьшается до ', counter.prev());
-console.log('Счетчик уменьшается до ', counter.prev());
-console.log('Счетчик увеличивается до ', counter.next());
+console.log('Счетчик увеличивается до', counter.next());
+console.log('Счетчик увеличивается до', counter.next());
+console.log('Счетчик увеличивается до', counter.next());
+console.log('Счетчик увеличивается до', counter.next());
+console.log('Счетчик увеличивается до', counter.next());
+console.log('Счетчик уменьшается до', counter.prev());
+console.log('Счетчик уменьшается до', counter.prev());
+console.log('Счетчик уменьшается до', counter.prev());
+console.log('Счетчик уменьшается до', counter.prev());
+console.log('Счетчик увеличивается до', counter.next());
 console.log('В памяти сохранены значения', counter.history());
-console.log('Счетчик увеличивается до ', counter.next());
-console.log('Счетчик увеличивается до ', counter.next());
-console.log('Счетчик увеличивается до ', counter.next());
+console.log('Счетчик увеличивается до', counter.next());
+console.log('Счетчик увеличивается до', counter.next());
+console.log('Счетчик увеличивается до', counter.next());
 console.log('В памяти сохранены значения', counter.history());
 
 //HT4------------------------------------------------
+
+function sumWith(number) {
+  return this.currentValue += number;
+}
+
+var number = 2;
+alert(sumWith(number));
+
+//Для дополнительного задания №1
+//1
+alert(sumWith.bind({ currentValue: 3}, number)());
+//2
+alert(sumWith.apply({ currentValue: 3 }, [number]));
+//3
+alert(sumWith.call({ currentValue: 3 }, number));
+
+//Для дополнительного задания №2
+function sumWith(number) {
+  return this.currentValue += number;
+}
+
+var number = 2;
+var test = sumWith.bind({ currentValue: -1 });
+alert(test(number));
+alert(test(number));
+alert(test(number));
+alert(test(number));
+alert(test(number));
+
+//HT5------------------------------------------------
+
+//Для задания 'a':
+var c = 0;
+var i = setInterval(function() { 
+	console.log('Random text'); 
+	c++; 
+	}, 2000);
+setInterval(function() { 
+	if (c >=5) { 
+		clearInterval(i); 
+		} 
+	});
+
+
+//Для задания 'b':
+function timeout(n) {
+  setTimeout(function() {
+    console.log('Random text');
+    timeout(n+2000);
+  }, n);
+}
+
+timeout(1);
 
